b7058480a6c47e219fb318c9eae6f92b7c2ef325
rkuo2000 <devnull@localhost>
2016-10-14 08:19:45 +0000

    ADC0 reading gas sensor

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// NuMaker-PFM-NUC472 use ADC0 (A0) to read gas sensor (MQ-3)
+#include "mbed.h"
+
+AnalogIn   adc0(A0); // connected to MQ-3 gas sensor
+DigitalOut led(LED1);
+
+int main(void)
+{
+    printf("ADC0 start reading...\n\r");
+    while (1) { 
+        printf("ADC0 value= 0x%4x, percentage= %3.3f%%\n\r",adc0.read_u16(), adc0.read()*100.0f);
+        if(adc0 > 0.7f) led = 0; // > 0.7 * Vcc wil turn on led
+        else            led = 1;
+        wait(0.2f);
+    }
+}
