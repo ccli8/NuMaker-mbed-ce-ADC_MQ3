5271ae3aaf651505b1fd4507fa3bfeea80c94057
SHLIU1 <SHLIU1@OANBE02333.nuvoton.com>
2021-02-24 16:27:23 +0800

    Support the both V5.X and V6.X

--- main.cpp
+++ main.cpp
@@ -8,11 +8,19 @@ DigitalOut led(LED1);
 
 int main(void)
 {
+#ifdef MBED_MAJOR_VERSION
+    printf("Mbed OS version %d.%d.%d\r\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+#endif
     printf("ADC0 start reading...\n\r");
     while (1) { 
         printf("ADC0 value= 0x%4x, percentage= %3.3f%%\n\r",adc0.read_u16(), adc0.read()*100.0f);
         if(adc0 > 0.7f) led = 0; // > 0.7 * Vcc wil turn on led
         else            led = 1;
+#if MBED_MAJOR_VERSION >= 6 
+	ThisThread::sleep_for(200);
+#else
         wait(0.2f);
+#endif
+
     }
 }
